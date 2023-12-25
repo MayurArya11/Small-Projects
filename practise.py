b98d611e660f8ff0f8ac5dc8e6d4158b498652eb
DELL <mayur.arya11@gmail.com>
2023-12-25 21:04:40 +0530

    Delete file

--- practise.py
+++
@@ -1,8 +0,0 @@
-for i in range(6):
-    for k in range(i):
-        print(" ", end=" ")
-    for j in range(6-i):
-        print("*", end=" ")
-    for l in range(6-i-1):
-       print("*", end=" ")
-    print()
